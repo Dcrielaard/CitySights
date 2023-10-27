c6061e8acb88a7aa194d111c4dbc05cfb277c830
Dennis Crielaard <denniscrielaard@icloud.com>
2023-10-27 22:49:58 +0200

    Add config file with Yelp API key

--- CitySights/ContentView.swift
+++ CitySights/ContentView.swift
@@ -21,6 +21,9 @@ struct ContentView: View {
             }
         }
         .padding()
+        .onAppear(perform: {
+            print(Bundle.main.infoDictionary?["API_KEY"] as? String)
+        })
     }
 }
 
