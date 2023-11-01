08cb0065a788af872f54ad5624a295b1218b3433
Dennis Crielaard <denniscrielaard@icloud.com>
2023-11-01 12:23:47 +0100

    Create list with names of businesses

--- CitySights/ContentView.swift
+++ CitySights/ContentView.swift
@@ -8,24 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var businesses = [Business]()
     @State var query: String = ""
     var service = DataService()
     
     var body: some View {
-        HStack {
-            TextField("What are you looking for?", text: $query)
-                .textFieldStyle(.roundedBorder)
-            
-            Button {
-                // TODO: Implement button query
-            } label: {
-                Text("Go")
+        VStack {
+            HStack {
+                TextField("What are you looking for?", text: $query)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button {
+                    // TODO: Implement button query
+                } label: {
+                    Text("Go")
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
             }
+            
+            List(businesses) { business in
+                Text(business.name ?? "Nil")
+                
+            }
+
         }
         .padding()
         .task {
-                let business = await service.businessSearch()
-            }
+                businesses = await service.businessSearch()
+    }
     }
 }
 
