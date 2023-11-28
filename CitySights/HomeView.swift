dd7e96e89e76b538b119bca06bc5db1da21c437a
Dennis Crielaard <denniscrielaard@icloud.com>
2023-11-28 20:57:47 +0100

    Add markers to mapview

--- CitySights/HomeView.swift
+++ CitySights/HomeView.swift
@@ -14,6 +14,7 @@ struct HomeView: View {
     
     var body: some View {
         @Bindable var model = model
+
         VStack {
             HStack {
                 TextField("What are you looking for?", text: $model.query)
